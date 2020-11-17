ae37cab41a70757314d8d17a3c93f94b0eea158f
Ashley Pauldo <ashleypauldo@macbook-pro-4.lan>
2020-11-16 21:59:44 -0500

    first commit

---
+++ easy.js
@@ -0,0 +1,20 @@
+//var run = exercise('running');
+//console.log(run()); // prints "Today's exercise: running"
+//var swim = exercise('swimming');
+//console.log(swim()); // prints "Today's exercise: swimming"
+
+function running(){
+    var exercise = "Today's exericse is";
+    return exercise;
+}
+
+var exercise = "running"
+console.log(exercise("running"));
+
+function swimming(){
+    var exercise = "Today's exercise is";
+    return exercise;
+}
+
+var exercise = "swimming"
+console.log(exercise("swimming"));
